3d09360eb92d38e54ad55ef6dccfa69b0a5645c4
bendert8000 <144204916+bendert8000@users.noreply.github.com>
2023-09-19 13:55:06 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -1895,7 +1895,8 @@ class LsportApiController extends Controller {
         if ($return) {
             //---------------------------------
             // 取得代理的語系
-            $player_id2 = $session['player']['id'];
+            //$player_id2 = $session['player']['id'];
+            $player_id2 = Session::get('player.id');
             $agentlang = $this->getAgentLang($player_id2);
             if ($agentlang === false) {
                 //$this->error(__CLASS__, __FUNCTION__, "02");
