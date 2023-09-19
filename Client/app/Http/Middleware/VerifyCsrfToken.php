3c595f813453e59d0e040aef72caf60a3d7c0c42
bendert8000 <144204916+bendert8000@users.noreply.github.com>
2023-09-19 11:29:41 +0800

    Update VerifyCsrfToken.php

--- Client/app/Http/Middleware/VerifyCsrfToken.php
+++ Client/app/Http/Middleware/VerifyCsrfToken.php
@@ -43,6 +43,22 @@ class VerifyCsrfToken extends Middleware
             '/api/v1/game_bet',
             '/api/v1/m_game_bet',
             '/api/v1/balance_logs',
+            //////////////////////////
+
+            '/api/v2/common_account',
+            '/api/v2/common_order',  
+            '/api/v2/index_carousel',
+            '/api/v2/index_marquee', 
+            '/api/v2/index_notice', 
+            '/api/v2/index_match_list', 
+            '/api/v2/result_index', 
+            '/api/v2/match_index',
+            '/api/v2/match_sport',
+            '/api/v2/match_list',
+            '/api/v2/game_index',
+            '/api/v2/game_bet',
+            '/api/v2/m_game_bet',
+            '/api/v2/balance_logs',
 
             //////////////////////////
 
