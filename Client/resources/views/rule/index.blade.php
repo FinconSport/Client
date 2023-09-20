51965febe1eb6ab4cf34f0c507ed7144e8524b8f
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-09-20 16:17:56 +0800

    update rule/index.blade.php

--- Client/resources/views/rule/index.blade.php
+++ Client/resources/views/rule/index.blade.php
@@ -20,18 +20,6 @@
                                 <h2>{{ trans('rule.ruleTitles.baseball') }}</h2>
                                 <h3>{{ trans('rule.ruleTitles.general_rule') }}</h3>
                                 <ul class="number-bullets">
-                                    @foreach(trans('rule.generalRulesBaseball') as $key => $gr_baseball)
-                                        <li>
-                                            {{ $gr_baseball }}
-                                            @if(is_array($gr_baseball))
-                                                <ul>
-                                                    @foreach($gr_baseball as $subKey => $subValue)
-                                                        <li>{{ $subValue }}</li>
-                                                    @endforeach
-                                                </ul>
-                                            @endif
-                                        </li>
-                                    @endforeach
                                     <li>{{ trans('rule.generalRulesBaseball.gr_baseball_1') }}</li>
                                     <li>{{ trans('rule.generalRulesBaseball.gr_baseball_2') }}</li>
                                     <li>{{ trans('rule.generalRulesBaseball.gr_baseball_3') }}</li>
@@ -43,14 +31,14 @@
                                     <li>{{ trans('rule.generalRulesBaseball.gr_baseball_9') }}</li>
                                     <li>{{ trans('rule.generalRulesBaseball.gr_baseball_10') }}</li>
                                     <li>{{ trans('rule.generalRulesBaseball.gr_baseball_11') }}</li>
-                                    <!-- <ul class="number-bullets">
+                                    <ul class="number-bullets">
                                         <li>{{ trans('rule.generalRulesBaseball.gr_baseball_11_1') }}</li>
                                         <li>{{ trans('rule.generalRulesBaseball.gr_baseball_11_2') }}</li>
                                         <ul class="no-bullets">
                                             <li>{{ trans('rule.generalRulesBaseball.gr_baseball_11_2_1') }}</li>
                                             <li>{{ trans('rule.generalRulesBaseball.gr_baseball_11_2_2') }}</li>
                                         </ul>
-                                    </ul> -->
+                                    </ul>
                                 </ul>
                                 <hr class="solid">
                                 <h2>{{ trans('rule.ruleTitles.betting_type') }}</h2>
