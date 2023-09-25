2f684f2475d3952c42247e9c34b4bff187a2bdd8
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-09-26 03:22:10 +0800

    app.blade.php

--- Client/resources/views/layout/app.blade.php
+++ Client/resources/views/layout/app.blade.php
@@ -396,17 +396,19 @@
 
 		// left side menu click function
 		$(document).ready(function(){
-			$(".submenu-btn").click(function () {
-				var submenuToggleList = $(this).next(".submenu-toggle-list");
-				$(this).closest('.submenu-main').toggleClass('active');
-				if (submenuToggleList.css('max-height') === '0px') {
-					submenuToggleList.css('max-height', '900px');
-				} else {
-					submenuToggleList.css('max-height', '0px');
-				}
+			$(".submenu-btn").click(function(){
+                $(this).closest('.submenu-main').toggleClass('active');
+                var submenuToggleList = $(this).next(".submenu-toggle-list");
+                if (submenuToggleList.length) {
+                    if (submenuToggleList[0].style.maxHeight === '0px' || submenuToggleList[0].style.maxHeight === '') {
+                        submenuToggleList[0].style.maxHeight = submenuToggleList[0].scrollHeight + 'px';
+                    } else {
+                        submenuToggleList[0].style.maxHeight = '0';
+                    }
+                }
 				$('.submenu-main').not($(this).closest('.submenu-main')).removeClass("active");
-				$('.submenu-toggle-list').not(submenuToggleList).css('max-height', '0px');
-			});
+				$('.submenu-toggle-list').not(submenuToggleList[0]).css('max-height', '0');
+            });
 
 			// Toggle 'openToggle' class for sport select elements
 			$(".sportSelect").click(function(){
