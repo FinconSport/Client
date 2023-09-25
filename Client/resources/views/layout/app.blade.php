bd000d857709cc0668174ddc57ec19dddf5647f9
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-09-25 12:13:26 +0800

    Update app.blade.php

--- Client/resources/views/layout/app.blade.php
+++ Client/resources/views/layout/app.blade.php
@@ -263,11 +263,11 @@
 			const currentUrl = window.location.href;
 			const noPath = window.location.pathname;
 
-			if (currentUrl.includes('index') || noPath == '') {
+			if (currentUrl.includes('index') || noPath == '/') {
 				sportType = 1; // Update sportType to 1 based on conditions
 			}
 
-			if (noPath == '') {
+			if (noPath == '/') {
 				console.log(noPath); // Log 'noPath' when the path is empty
 			}
 
