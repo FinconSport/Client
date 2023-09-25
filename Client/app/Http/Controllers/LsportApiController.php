01de2f1a0b0792e8175051139f9e8a73e8ac4081
FinConSoft <fayi.pon@gmail.com>
2023-09-25 15:56:09 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -1464,9 +1464,17 @@ class LsportApiController extends Controller {
             $scoreboard[] = $d;
 
             // 局數 
+            $d = array();
             $tmp = json_decode($v['periods'],true);
 
             dd($tmp);
+            foreach ($tmp['Results'] as $kk => $vv) {
+                $pos = $vv['Position'];
+                $d[$pos] = $vv['Value'];
+            }
+            $scoreboard[] = $d;
+
+            dd($scoreboard);
 
             ////////////
 
