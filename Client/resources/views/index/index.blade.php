7e0bf6d7f0e93aa71257376d5c3a199e8efb9541
36peco <41123173+36peco@users.noreply.github.com>
2023-09-26 18:24:49 +0800

    test

--- Client/resources/views/index/index.blade.php
+++ Client/resources/views/index/index.blade.php
@@ -232,8 +232,6 @@
             el_toggle.removeAttr('hidden')
             el_toggle.removeAttr('template')
 
-            console.log(v)
-            console.log(sport)
             Object.entries(v[sport].list).map(([k2, v2]) => { // league toggle
                 let league_toggle = $('div[template="leagueToggleTitleTemplate"]').clone()
                 let league_toggle_name = league_toggle.find('.legToggleName')
