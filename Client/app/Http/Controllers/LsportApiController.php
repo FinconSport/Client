bbb2bb01f173125802c9e88360ad08f915c6d1d7
bendert8000 <144204916+bendert8000@users.noreply.github.com>
2023-09-27 17:17:22 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -750,11 +750,11 @@ class LsportApiController extends Controller {
 
         ///////////////////////////////
         // gzip
-        // $data = $this->gzip($data);
+        $data = $this->gzip($data);
 
-        // $this->ApiSuccess($data, "01", true);
+        $this->ApiSuccess($data, "01", true);
         
-        $this->ApiSuccess($data, "01", false);
+        // $this->ApiSuccess($data, "01", false);
     }
 
     /**
