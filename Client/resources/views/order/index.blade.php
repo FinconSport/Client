cd8fb9cfd9c2d75d51e388e86a2bbea894502e65
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-09-28 15:18:15 +0800

    Update index.blade.php

--- Client/resources/views/order/index.blade.php
+++ Client/resources/views/order/index.blade.php
@@ -262,11 +262,9 @@
 		// Set content for the found elements
 		betDataDetails_leagueName.html(betItem.league_name);
 		betDataDetails_HomeName.html(betItem.home_team_name);
-		var homeTeamScore = {!! $betItem->home_team_score !== null ? json_encode('(' . $betItem->home_team_score . ')') : "''" !!};
-    	betDataDetails_HomeScore.html('(' + homeTeamScore + ')');
+		betDataDetails_HomeScore.html('(' + betItem.home_team_score + ')');
 		betDataDetails_AwayName.html(betItem.away_team_name);
-		var awayTeamScore = {!! $betItem->away_team_score !== null ? json_encode('(' . $betItem->away_team_score . ')') : "''" !!};
-    	betDataDetails_AwayScore.html('(' + awayTeamScore + ')');
+		betDataDetails_AwayScore.html('(' + betItem.away_team_score +')');
 		betDataDetails_BetNameLine.html(betItem.market_name + betItem.market_bet_name + betItem.market_bet_line);
 		betDataDetails_BetRate.html('@' + betItem.bet_rate);
 		betDataDetails_BetStatus.html(betItem.bet_status);
