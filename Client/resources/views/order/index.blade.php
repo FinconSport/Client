b04fe5939371b1285d54f41d8b9111a7b28a0dc0
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-09-28 13:18:08 +0800

    Update index.blade.php

--- Client/resources/views/order/index.blade.php
+++ Client/resources/views/order/index.blade.php
@@ -480,28 +480,28 @@
 	}
 	
 	
-	const sportListD = {
-		"status": 1,
-		"data": [
-			{
-				"sport_id": 154914,
-				"name": "棒球",
-				"key": 1
-			},
-			{
-				"sport_id": 6046,
-				"name": "足球",
-				"key": 2
-			},
-			{
-				"sport_id": 48242,
-				"name": "籃球",
-				"key": 3
-			}
-		],
-		"message": "SUCCESS_API_MATCH_SPORT_01",
-		"gzip": false
-	};
+	// const sportListD = {
+	// 	"status": 1,
+	// 	"data": [
+	// 		{
+	// 			"sport_id": 154914,
+	// 			"name": "棒球",
+	// 			"key": 1
+	// 		},
+	// 		{
+	// 			"sport_id": 6046,
+	// 			"name": "足球",
+	// 			"key": 2
+	// 		},
+	// 		{
+	// 			"sport_id": 48242,
+	// 			"name": "籃球",
+	// 			"key": 3
+	// 		}
+	// 	],
+	// 	"message": "SUCCESS_API_MATCH_SPORT_01",
+	// 	"gzip": false
+	// };
 
 	const specificSport = sportListD.data.find(item => item.sport_id === sport);
 
