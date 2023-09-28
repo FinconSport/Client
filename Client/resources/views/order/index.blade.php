c7bca64f4c66b5cbca90bc0bf68d6254d4875a02
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-09-29 03:14:20 +0800

    Update index.blade.php

--- Client/resources/views/order/index.blade.php
+++ Client/resources/views/order/index.blade.php
@@ -272,7 +272,7 @@
 
 		// Append the container to the orderDataBetDataDetails
 		orderDataBetDataDetails.append(betDataDetailsContainer);
-		console.log('count:' + betIndex.length);
+		console.log('count:' + betIndex);
 
 		if (betIndex === 0) { // Check if it's the first item
 			var button = $("<button class='order-toggleButton'>{{ trans('order.main.expand') }}</button>");
