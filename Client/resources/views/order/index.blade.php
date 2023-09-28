2703d4d6ba2fd0ead0bf1b888f2176764d977745
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-09-28 13:17:16 +0800

    Update index.blade.php

--- Client/resources/views/order/index.blade.php
+++ Client/resources/views/order/index.blade.php
@@ -480,25 +480,37 @@
 	}
 	
 	
-	if (sportListD && sportListD.data) {
-		var sportList = sportListD.data;
+	const sportListD = {
+		"status": 1,
+		"data": [
+			{
+				"sport_id": 154914,
+				"name": "棒球",
+				"key": 1
+			},
+			{
+				"sport_id": 6046,
+				"name": "足球",
+				"key": 2
+			},
+			{
+				"sport_id": 48242,
+				"name": "籃球",
+				"key": 3
+			}
+		],
+		"message": "SUCCESS_API_MATCH_SPORT_01",
+		"gzip": false
+	};
 
-		// Log the sportList to inspect its contents
-		console.log("Sport List:", sportList);
+	const specificSport = sportListD.data.find(item => item.sport_id === sport);
 
-		// Find the specific sport with ID 6046
-		var specificSport = sportList.find(item => item.sport_id === 6046);
-
-		// Check if the specific sport was found
-		if (specificSport) {
-			console.log("Sport ID:", specificSport.sport_id);
-			console.log("Name:", specificSport.name);
-			console.log("Key:", specificSport.key);
-		} else {
-			console.log("Sport with ID 6046 not found.");
-		}
+	if (specificSport) {
+		console.log("Sport ID:", specificSport.sport_id);
+		console.log("Name:", specificSport.name);
+		console.log("Key:", specificSport.key);
 	} else {
-		console.log("Sport List data not available.");
+		console.log("Sport with ID not found.");
 	}
 
 
