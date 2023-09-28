5a71e6e8e6e7b2cc61df71dc27a26d5324324e4f
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-09-28 17:27:03 +0800

    Update index.blade.php

--- Client/resources/views/order/index.blade.php
+++ Client/resources/views/order/index.blade.php
@@ -265,8 +265,8 @@
 		if (betIndex > 0) {
 			// Add a custom class to elements of subsequent bet_data items
 			betDataDetailsContainer.addClass('custom-class'); // Change 'custom-class' to your desired class name
-			let button = $('<button class="btn btn-primary">Show more</button>'); // Replace "Button Text" with your desired button text
-			$('.custom-class').after(button);
+			let button = $('<button class="">Show More</button>'); // Replace "Button Text" with your desired button text
+			orderDataBetDataDetails.append(button);
 		}
 
 		// Append the container to the orderDataBetDataDetails
