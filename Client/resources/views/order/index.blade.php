224fd1e1cadc49c3c8eb44a55ec4c95a91b83cbc
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-03 10:21:32 +0800

    Update index.blade.php

--- Client/resources/views/order/index.blade.php
+++ Client/resources/views/order/index.blade.php
@@ -141,17 +141,14 @@
 
 		let sportName = '';
 
-		for (const item of orderListD.data.list) {
-			for (const bet of item.bet_data) {
+		for (const bet of orderItem.bet_data) {
 			const matchingSport = sportListD.data.find(sport => sport.sport_id === bet.sport_id);
-			// sportName = matchingSport ? matchingSport.name : '';
-			sportName = matchingSport.name;
-			console.log(bet.sport_id + ' sportname ' + matchingSport.name);
-			}
+			sportName = matchingSport ? matchingSport.name : '';
+			console.log(bet.sport_id + ' sportname ' + sportName);
+			orderDataSportType.html(sportName);
 		}
 
 		orderDataId.html(orderItem.m_order === 1 ? orderItem.m_id : orderItem.id);
-		orderDataSportType.html(sportName);
 		orderDataMOrder.html(orderItem.m_order === 0 ? '{{ trans("order.main.sport") }}' : '{{ trans("order.main.morder") }}');
 		orderDataBetDataDetails.attr('id', `betDataDetails_${orderItem.id}`);
 		orderDataBetAmount.html(orderItem.bet_amount);
