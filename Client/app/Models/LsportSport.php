eec6abfd3ccc6d3d8360e6a2144828c40751e163
FinConSoft <fayi.pon@gmail.com>
2023-10-06 07:30:23 +0800

    Update LsportSport.php

--- Client/app/Models/LsportSport.php
+++ Client/app/Models/LsportSport.php
@@ -13,6 +13,28 @@ class LsportSport extends CacheModel
 	public $timestamps = false;
 	protected $table = "lsport_sport";
 
+    public static function getName($data) {
+
+        // 緩存時間
+        $cacheAliveTime = 3600;
+        // 緩存Key
+        $lsportSport = new static;
+        $cacheKey = $lsportSport->getCacheKey($data);
+		
+		$sport_id = $data['sport_id'];
+		$api_lang = 'tw';
+
+        return Cache::remember($cacheKey, $cacheAliveTime, function () use ($sport_id, $api_lang, $lsportSport) {
+            $data = $lsportSport->where('sport_id', $sport_id)->first();
+            // 默认名称
+            $name = $data['name_en'];
+            if (($data['name_' . $api_lang] != "") && ($data['name_' . $api_lang] != null)) {
+                $name = $data['name_' . $api_lang];
+            }
+            return $name;
+        });
+    }
+
 	// 取得Sport Name
     public static function getName($data) {
 
