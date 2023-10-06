a5689d79221f48f8d4ffeb2c6376eda689550d9b
bendert8000 <144204916+bendert8000@users.noreply.github.com>
2023-10-06 15:43:42 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -298,8 +298,8 @@ class LsportApiController extends Controller {
                 $notice_type = "{$arr_notice_type[0]} {$arr_notice_type[1]}";
                 $fixture_id = $arr_notice_type[2];
             }
-            $title = __('Title:'.$notice_type);
-            $context = __($notice_type, ['fixture_id' => $fixture_id]);
+            $title = trans('Title:'.$notice_type);
+            $context = trans($notice_type, ['fixture_id' => $fixture_id]);
             $create_time = $v['create_time'];
 
             $notice_list[$sport_id][] = [
