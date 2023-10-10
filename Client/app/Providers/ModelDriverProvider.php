80f40fd280e2ed5d2a56764d40d2b78bf57fcb3a
FinConSoft <fayi.pon@gmail.com>
2023-10-10 19:34:01 +0800

    Update ModelDriverProvider.php

--- Client/app/Providers/ModelDriverProvider.php
+++ Client/app/Providers/ModelDriverProvider.php
@@ -13,7 +13,7 @@ class ModelDriverProvider extends ServiceProvider {
     }
 
     public function boot() {
-        Builder::macro('list', function use ($cacheAliveTime = 3600) {
+        Builder::macro('list', function ($cacheAliveTime = 3600) {
         
             // getSQL
             $bindings = $this->getBindings();
