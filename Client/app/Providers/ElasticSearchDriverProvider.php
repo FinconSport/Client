7b724275fcb8ed00ae62737fe63e4c9c713ef5e5
FinConSoft <fayi.pon@gmail.com>
2023-10-10 21:54:19 +0800

    Update ElasticSearchDriverProvider.php

--- Client/app/Providers/ElasticSearchDriverProvider.php
+++ Client/app/Providers/ElasticSearchDriverProvider.php
@@ -131,12 +131,15 @@ class ElasticSearchDriverProvider extends ServiceProvider {
                     foreach ($data['aggregations'] as $k => $v) { 
                         foreach ($data['aggregations'] as $kk => $vv) {
                             $buckets = $vv['buckets'];
+                            $count = 0; // 用於計算欄位順序
                             foreach ($buckets as $kkk => $vvv) {
                                 $tmp = array();
                                 $tmp['agent_id'] = $vvv['key'];
-                                if (isset($vvv['total'])) {
-                                    $tmp['total'] = $vvv['total'];
+                                if ($count == 2) {
+                                    $tmp['value'] = $vvv[$kkk]['value'];
                                 }
+                                $count++;
+                                
                                 $list[] = $tmp;
                             }
                         }
