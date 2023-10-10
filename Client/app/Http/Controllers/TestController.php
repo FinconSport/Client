0e10bbebd9d90988f41e7614f4a38b821db54f5a
FinConSoft <fayi.pon@gmail.com>
2023-10-10 17:52:56 +0800

    Update TestController.php

--- Client/app/Http/Controllers/TestController.php
+++ Client/app/Http/Controllers/TestController.php
@@ -24,7 +24,8 @@ class TestController extends PcController {
       // $dd = LsportSport::getESQuery($sql);
       //$dd = LsportSport::getESAgg($sql);
 
-      $return = Player::where("status",1)->list();
+      $return = Player::where("status",1);
+      $return = $return->list();
       dd($return);
       
 
