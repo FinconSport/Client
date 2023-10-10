bdb9ae4747cf249627b005e4ad4ded644ad49246
bendert8000 <144204916+bendert8000@users.noreply.github.com>
2023-10-10 10:10:51 +0800

    cache

--- Client/app/Models/Player.php
+++ Client/app/Models/Player.php
@@ -14,9 +14,8 @@ class Player extends CacheModel
 	protected $table = "player";
 
     public static function getData(
-        $data,  // data=參數, 
-        $id_col = 'id',  // id_col=主鍵或是搜尋的欄位名
-        $col = null // col=要取出的欄位(字串或陣列),未指定則回傳整個array
+        array $data,  // data=參數, 
+        string $id_col = 'id'  // id_col=主鍵或是搜尋的欄位名
     ) {
 
         // 緩存時間
@@ -25,17 +24,13 @@ class Player extends CacheModel
         // 緩存Key
         $cacheKey = (new static)->getCacheKey($data , __FUNCTION__);
 
-        return Cache::remember($cacheKey, $cacheAliveTime, function () use ($data, $id_col, $col) {
+        return Cache::remember($cacheKey, $cacheAliveTime, function () use ($data, $id_col) {
             if (!strlen($id_col)) {
                 $id_col = 'id';
             }
             $id = $data[$id_col];
 
-            if ($col && strlen($col)) {
-                $data = self::where($id_col, $id)->select($col)->first();
-            } else {
-                $data = self::where($id_col, $id)->first();
-            }
+            $data = self::where($id_col, $id)->first();
             $return = $data;
 
             return $return;
