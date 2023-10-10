75df63a54fac4013474044b6f117dc2c413e3ff1
bendert8000 <144204916+bendert8000@users.noreply.github.com>
2023-10-10 10:02:45 +0800

    cache

--- Client/app/Models/Player.php
+++ Client/app/Models/Player.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
 
 class Player extends CacheModel
@@ -13,7 +13,11 @@ class Player extends CacheModel
 	public $timestamps = false;
 	protected $table = "player";
 
-    public static function getData($data, $col = null) {
+    public static function getData(
+        $data,  // data=參數, 
+        $id_col = 'id',  // id_col=主鍵或是搜尋的欄位名
+        $col = null // col=要取出的欄位(字串或陣列),未指定則回傳整個array
+    ) {
 
         // 緩存時間
         $cacheAliveTime = 1;
@@ -21,13 +25,16 @@ class Player extends CacheModel
         // 緩存Key
         $cacheKey = (new static)->getCacheKey($data , __FUNCTION__);
 
-        return Cache::remember($cacheKey, $cacheAliveTime, function () use ($data, $col) {
-			$player_id = $data['player_id'];
+        return Cache::remember($cacheKey, $cacheAliveTime, function () use ($data, $id_col, $col) {
+            if (!strlen($id_col)) {
+                $id_col = 'id';
+            }
+            $id = $data[$id_col];
 
             if ($col && strlen($col)) {
-                $data = self::where('id', $player_id)->select($col)->first();
+                $data = self::where($id_col, $id)->select($col)->first();
             } else {
-                $data = self::where('id', $player_id)->first();
+                $data = self::where($id_col, $id)->first();
             }
             $return = $data;
 
