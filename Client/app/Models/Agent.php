579c6a6e71cb1d7da864b859f071bbee246101b0
bendert8000 <144204916+bendert8000@users.noreply.github.com>
2023-10-10 10:45:08 +0800

    Update Agent.php

--- Client/app/Models/Agent.php
+++ Client/app/Models/Agent.php
@@ -13,48 +13,48 @@ class Agent extends CacheModel
 	public $timestamps = false;
 	protected $table = "agent";
 
-    public static function getApiLang(
-        array $data  // data=參數
-    ) {
-
-        // 緩存時間
-        $cacheAliveTime = 3600;
-
-        // 緩存Key
-        $cacheKey = (new static)->getCacheKey($data , __FUNCTION__);
-
-        return Cache::remember($cacheKey, $cacheAliveTime, function () use ($data) {
-            $id = $data['id'];
-
-            $data = self::where('id', $id)->select('api_lang')->first();
-            $return = $data;
-
-            return $return;
-        });
-    }
-
-    // public static function getData(
-    //     array $data,  // data=參數, 
-    //     string $id_col = 'id'  // id_col=主鍵或是搜尋的欄位名
+    // public static function getApiLang(
+    //     array $data  // data=參數
     // ) {
 
     //     // 緩存時間
-    //     $cacheAliveTime = 1;
+    //     $cacheAliveTime = 3600;
 
     //     // 緩存Key
     //     $cacheKey = (new static)->getCacheKey($data , __FUNCTION__);
 
-    //     return Cache::remember($cacheKey, $cacheAliveTime, function () use ($data, $id_col) {
-    //         if (!strlen($id_col)) {
-    //             $id_col = 'id';
-    //         }
-    //         $id = $data[$id_col];
+    //     return Cache::remember($cacheKey, $cacheAliveTime, function () use ($data) {
+    //         $id = $data['id'];
 
-    //         $data = self::where($id_col, $id)->first();
+    //         $data = self::where('id', $id)->select('api_lang')->first();
     //         $return = $data;
 
     //         return $return;
     //     });
     // }
+
+    public static function getData(
+        array $data,  // data=參數, 
+        string $id_col = 'id'  // id_col=主鍵或是搜尋的欄位名
+    ) {
+
+        // 緩存時間
+        $cacheAliveTime = 1;
+
+        // 緩存Key
+        $cacheKey = (new static)->getCacheKey($data , __FUNCTION__);
+
+        return Cache::remember($cacheKey, $cacheAliveTime, function () use ($data, $id_col) {
+            if (!strlen($id_col)) {
+                $id_col = 'id';
+            }
+            $id = $data[$id_col];
+
+            $data = self::where($id_col, $id)->first();
+            $return = $data;
+
+            return $return;
+        });
+    }
 
 }
