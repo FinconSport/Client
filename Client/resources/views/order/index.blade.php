3896d4fca162f0e1f344afc56dee40e398275662
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-11 12:06:32 +0800

    fix css

--- Client/resources/views/order/index.blade.php
+++ Client/resources/views/order/index.blade.php
@@ -4,7 +4,7 @@
 	<div class="search-statistic-container">
 		<div class="search-bar-container">
 			<div class="select-con">
-			<select id="selectOption" name="selectOption" onfocus="this.size=2;" onchange="redirectToPage(this)">
+			<select id="selectOption" name="selectOption"  onchange="redirectToPage(this)">
 				<option value="" disabled></option>
 				<option value="{{ trans('common.left_menu.unsettled') }}" data-link="?result=0">{{ trans('common.left_menu.unsettled') }}</option>
 				<option value="{{ trans('common.left_menu.settled') }}" data-link="?result=1">{{ trans('common.left_menu.settled') }}</option>
