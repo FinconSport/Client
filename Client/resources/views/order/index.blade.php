c62749d7e2eab7f6e967aa2e2bea3684cb366ae8
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-12 13:15:42 +0800

    Update index.blade.php

--- Client/resources/views/order/index.blade.php
+++ Client/resources/views/order/index.blade.php
@@ -477,6 +477,8 @@
 		if (link) {
 			window.location.search = link;
 		}
+
+		adjustContainerHeight();
 	}
 
     
@@ -502,7 +504,7 @@
 		var orderTableHeight = $('#orderTable').height();
 
 		// Check if the height is less than 877px
-		if (orderTableHeight < 877) {
+		if (orderTableHeight < 870) {
 			$('#orderContainer').css('height', 'auto');
 		} else {
 			// Set the height of #orderContainer to 'calc(100% - 9.5rem)'
