bd73f0576236942b88db4d3e3cc9c43e93072ae8
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-12 14:43:21 +0800

    Update index.blade.php

--- Client/resources/views/index/index.blade.php
+++ Client/resources/views/index/index.blade.php
@@ -1279,6 +1279,12 @@
             success: function(response) {
                 let res = JSON.parse(response)
                 console.log(res)
+                let hasTenSecondsPassed = false;
+                // if the msg is not getting in 10 sec, hide the loading and close the betting area
+                setTimeout(function() {
+                    hideLoading();
+                    closeCal();
+                }, 10000);
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 console.error('error');
@@ -1286,18 +1292,6 @@
                 showErrorToast(jqXHR)
             }
         });
-
-        
-
-        let hasTenSecondsPassed = false;
-        // if the msg is not getting in 10 sec, hide the loading and close the betting area
-        setTimeout(function() {
-            hasTenSecondsPassed = true;
-            if (hasTenSecondsPassed) {
-                hideLoading();
-                closeCal();
-            }
-        }, 10000);
     }
 
     // 統計
