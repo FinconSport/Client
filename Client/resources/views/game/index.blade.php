de09e39d6eebe9dc8c4897b5620d7cd4be04900f
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-13 11:48:17 +0800

    css

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -277,6 +277,7 @@
 
 @section('styles')
 <link href="{{ asset('css/index.css?v=' . $system_config['version']) }}" rel="stylesheet">
+<link href="{{ asset('css/game.css?v=' . $system_config['version']) }}" rel="stylesheet">
 @endSection
 
 @push('main_js')
