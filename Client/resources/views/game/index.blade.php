f88f0c0f9f268387a604293bba1cc1a5d25dbb7b
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-13 14:15:47 +0800

    Update index.blade.php

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -277,7 +277,8 @@
 
 @section('styles')
 <link href="{{ asset('css/index.css?v=' . $system_config['version']) }}" rel="stylesheet">
-<link href="{{ asset('css/game.css?v=' . $system_config['version']) }}" rel="stylesheet">
+<link href="{{ asset('css/game.css?v=' . $current_time) }}" rel="stylesheet">
+<!-- <link href="{{ asset('css/game.css?v=' . $system_config['version']) }}" rel="stylesheet"> -->
 @endSection
 
 @push('main_js')
