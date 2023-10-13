5a35d2bd88c48bcf96058f55fc857ccc5eae9cb8
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-13 20:48:39 +0800

    Update index.blade.php

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -354,9 +354,9 @@
     /* ===== VIEW LAYER ===== */
     function viewIni() { // view ini
         // loop matchListD to generate html element here
-        // Object.entries(matchListD.data.list.market).map(([k, v]) => {  // living early toggle
-        //     console.log(k, v)
-        // })
+        Object.entries(matchListD.data.list.market).map(([k, v]) => {  // living early toggle
+            console.log(k, v)
+        })
     }
     /* ===== VIEW LAYER ===== */
 
