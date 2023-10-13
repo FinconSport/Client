fbcc4587d7aab652bdb678d542983e6bfa1e3ea3
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-13 13:23:18 +0800

    Update index.blade.php

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -619,7 +619,7 @@
 
         LeagueNameStartTime.removeAttr('hidden')
         LeagueNameStartTime.removeAttr('template')
-        LeagueNameStartTime.find('.leauge_name').html(sv)
+        LeagueNameStartTime.find('.leauge_name').html(sk.sv)
 
         $(`#homeTeamName`).after(LeagueNameStartTime)
     }
