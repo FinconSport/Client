94ecb577a6f82d7da35974068a0c5ee44aa29dab
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-13 14:33:42 +0800

    css

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -64,13 +64,13 @@
     {{ trans('common.search_area.search') }}
 </div>
 <div class="scoreboard-container" template='scoreBoardContainerTemplate' style="color:#ffffff;margin-top:20px;background-image: url('image/gameBg.jpg');" hidden>
-    <div class="fixture-card">
-        <p class="home_team_name"></p>
-        <div id="LeagueNameStartTime">
+    <div class="fixture-card row">
+        <p class="home_team_name col-2"></p>
+        <div id="LeagueNameStartTime col-10">
             <p class="league_name"></p>
             <p class="start_time"></p>
         </div>
-        <p class="away_team_name"></p>
+        <p class="away_team_name col-2"></p>
     </div>
 </div>
 
