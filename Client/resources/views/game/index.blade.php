8628f38512e6078c7795c3f6fac94696e6433e6e
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-14 03:12:57 +0800

    Update index.blade.php

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -699,7 +699,7 @@
             WebSocketDemo();
         }
     }
-    
+
     function renderViewV2() {
         if (matchListD.data.list.status === 1) {
             $('.marketName').css('background-color', '#c4d4d4');
@@ -770,15 +770,16 @@
                 marketBetRateTemp.find('.market_price').hide();
             }
 
+            let fixture_id = matchListD.data.list.fixture_id;
             if( v2.market_bet_id && v2.market_bet_id.toString() === (v2.market_bet_id).toString() && v2.status === 1 ) {
                 // 判斷賠率是否有改變
-                if( parseFloat(price) > parseFloat(v2.price) ) {
+                if( parseFloat(v2.price) > parseFloat(v2.price) ) {
                     // 賠率下降
-                    lowerOdd(k3, v.market_id, v2.market_bet_id)
+                    lowerOdd(k3, v.market_id, v2.market_bet_id, fixture_id)
                 }
                 if( parseFloat(v2.price) < parseFloat(v2.price) ) {
                     // 賠率上升
-                    raiseOdd(k2, v.market_id, v2.market_bet_id)
+                    raiseOdd(k2, v.market_id, v2.market_bet_id, fixture_id)
                 }
             } 
 
