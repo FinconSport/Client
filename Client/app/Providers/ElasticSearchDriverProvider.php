9b0f7c3efd8512c516b3ca347a7965dd71e64e14
FinConSoft <fayi.pon@gmail.com>
2023-10-13 15:42:57 +0800

    新增page 方法, 雖然client 用不到

--- Client/app/Providers/ElasticSearchDriverProvider.php
+++ Client/app/Providers/ElasticSearchDriverProvider.php
@@ -6,8 +6,11 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ElasticSearchDriverProvider extends ServiceProvider {
+
+    protected $totalCount = 0;
 
     public function register() {
         // ...
@@ -16,7 +19,7 @@ class ElasticSearchDriverProvider extends ServiceProvider {
     public function boot() {
 
         // list() method 
-        Builder::macro('list', function ($cacheAliveTime = 1) {
+        Builder::macro('list', function ($cacheAliveTime=1) {
             
             // get Model TableName
             $tableName = "`" . $this->getModel()->getTable() . "`";
@@ -55,9 +58,72 @@ class ElasticSearchDriverProvider extends ServiceProvider {
                 return false;
             });
         });
+
+        // page() method 
+        Builder::macro('page', function ($perPage=20, $cacheAliveTime=1) {
+            
+            // get Model TableName
+            $tableName = "`" . $this->getModel()->getTable() . "`";
+            $esTableName = "`es_" . $tableName."`";
+
+            // use skip and take
+            $page = request()->input('page', 1);
+
+            $skip = ($page - 1) * $perPage;
+            $take = $perPage;
         
+            $this->skip($skip)->take($take);
+            
+            // Build ES SQL
+            $bindings = $this->getBindings();
+            $rawSql = $this->toSql();
+            $esSql = vsprintf(str_replace('?', "'%s'", $rawSql), $bindings);    // getRawSQL
+            $esSql = str_replace($tableName, $esTableName, $esSql); // fix es_table_name
+            $esSql = str_replace("'", "", $esSql);  // remove '
+            $esSql = str_replace("`", "", $esSql);  // remove `
+            $cacheKey = MD5($esSql); // create CacheKey by MD5
+            // use Cache
+            $result = Cache::remember($cacheKey, $cacheAliveTime, function () use ($esSql) {
+                $url = 'http://72.167.135.22:29200/_sql?sql=' . $esSql . '&pretty';
+
+                $esUser = env("ES_USER");
+                $esPass = env("ES_PASS");
+
+                // Basic Auth
+                $response = Http::withBasicAuth($esUser, $esPass)->get($url);
+                
+                // check successful
+                if ($response->successful()) {
+                    // json decode
+                    $data = $response->json();
+
+                    // get TotalCount 
+                    $this->totalCount = $data['hits']['total'];
+
+                    $list = array();
+                    foreach ($data['hits']['hits'] as $k => $v) {
+                        $list[] = $v['_source'];
+                    }
+                    return $list;
+                } 
+                // fail , return false
+                return false;
+            });
+
+            // Create a LengthAwarePaginator instance
+            $paginator = new LengthAwarePaginator(
+                $result,
+                $this->totalCount,
+                $perPage,
+                $page,
+                ['path' => request()->url(), 'query' => request()->query()]
+            );
+
+            return $paginator;
+        });
+
         // cc() method 
-        Builder::macro('cc', function ($cacheAliveTime = 1) {
+        Builder::macro('cc', function ($cacheAliveTime=1) {
             
             // get Model TableName
             $tableName = "`" . $this->getModel()->getTable() . "`";
@@ -101,7 +167,7 @@ class ElasticSearchDriverProvider extends ServiceProvider {
         });
 
         // fetch() method 
-        Builder::macro('fetch', function ($cacheAliveTime = 1) {
+        Builder::macro('fetch', function ($cacheAliveTime=1) {
             
             // get Model TableName
             $tableName = "`" . $this->getModel()->getTable() . "`";
@@ -142,7 +208,7 @@ class ElasticSearchDriverProvider extends ServiceProvider {
         });
 
         // total() method 
-        Builder::macro('total', function ($cacheAliveTime = 1) {
+        Builder::macro('total', function ($cacheAliveTime=1) {
             
             // get Model TableName
             $tableName = "`" . $this->getModel()->getTable() . "`";
