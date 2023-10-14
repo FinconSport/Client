6ce3b3e446768e0b78b72c014d794a2739d2d00c
FinConSoft <fayi.pon@gmail.com>
2023-10-14 08:26:31 +0800

    Update ElasticSearchDriverProvider.php

--- Client/app/Providers/ElasticSearchDriverProvider.php
+++ Client/app/Providers/ElasticSearchDriverProvider.php
@@ -267,3 +267,11 @@ class ElasticSearchDriverProvider extends ServiceProvider {
     }
     
 }
+
+select 
+f.sport_id, 
+COUNT(f.*) as fixture_count 
+from es_lsport_fixture as f 
+inner join es_lsport_league as l on f.league_id = l.league_id 
+where l.status = 1 and f.status in (1, 2, 9) 
+group by f.sport_id
