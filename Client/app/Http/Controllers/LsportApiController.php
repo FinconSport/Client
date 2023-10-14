f73b4fb25dcbe391ad20f0529e6b00ba407859e8
FinConSoft <fayi.pon@gmail.com>
2023-10-14 10:16:42 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -490,6 +490,7 @@ class LsportApiController extends Controller {
         
         $return = LsportFixture::select('sport_id', 'status', DB::raw('COUNT(*) as count'))
         ->whereIn("status",[1,2,9])
+        ->where('start_time', "<=", $after_tomorrow)
         ->groupBy('sport_id', 'status')
         ->total();
         if ($return === false) {
