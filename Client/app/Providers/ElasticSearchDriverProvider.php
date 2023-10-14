6ab12e747c5670f904c1e36f73480e4158622bfc
FinConSoft <fayi.pon@gmail.com>
2023-10-14 08:23:58 +0800

    Update ElasticSearchDriverProvider.php

--- Client/app/Providers/ElasticSearchDriverProvider.php
+++ Client/app/Providers/ElasticSearchDriverProvider.php
@@ -32,7 +32,7 @@ class ElasticSearchDriverProvider extends ServiceProvider {
             $esSql = str_replace($tableName, $esTableName, $esSql); // fix es_table_name
             $esSql = str_replace("'", "", $esSql);  // remove '
             $esSql = str_replace("`", "", $esSql);  // remove `
-            dd($esSql);
+            //dd($esSql);
             $cacheKey = MD5($esSql); // create CacheKey by MD5
 
             // use Cache
@@ -222,6 +222,7 @@ class ElasticSearchDriverProvider extends ServiceProvider {
             $esSql = str_replace($tableName, $esTableName, $esSql); // fix es_table_name
             $esSql = str_replace("'", "", $esSql);  // remove '
             $esSql = str_replace("`", "", $esSql);  // remove `
+            dd($esSql);
             $cacheKey = MD5($esSql); // create CacheKey by MD5
 
             // use Cache
