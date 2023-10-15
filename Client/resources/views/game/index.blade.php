79f74eb503c7b78845e374a581979efd77efc820
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-15 21:41:44 +0800

    Update index.blade.php

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -735,8 +735,7 @@
             var sportId = matchListD.data.series.sport_id;
             var priority = v.priority;
             var transKey = 'game.game_priority.' + sportId + '.' + priority;
-            var translatedTitle = window.trans[transKey];
-            marketNameElement.html(`<i class="fa-sharp fa-solid fa-star" style="color: #415a5b; margin-right: 0.5rem;"></i> ${translatedTitle}`);
+            marketNameElement.html(`<i class="fa-sharp fa-solid fa-star" style="color: #415a5b; margin-right: 0.5rem;"></i> {{ trans('${transKey}') }}`);
             if (v.rate !== undefined && v.rate.length > 0) { //  If v.rate is empty or undefined, the append operation will not be performed.
                 $('#bettingTypeContainer').append(bettingTypeContainerTemp);
             }
