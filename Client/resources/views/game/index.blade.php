32514c3cd793382746046ca0f76df3349cb22d20
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-15 20:42:54 +0800

    Update index.blade.php

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -734,8 +734,9 @@
             const marketNameElement = bettingTypeContainerTemp.find('.market_name');
             const sportId = matchListD.data.series.sport_id;
             const priority = v.priority;
-            const translation = `'game.game_priority.` + ${sportId} + `.` + ${priority} + `'`;
-            marketNameElement.html(`<i class="fa-sharp fa-solid fa-star" style="color: #415a5b; margin-right: 0.5rem;"></i> {{ trans(${translation}) }}`);
+            const translation = 'game.game_priority.' + ${sportId} + '.' + ${priority};
+            console.log(translation);
+            // marketNameElement.html(`<i class="fa-sharp fa-solid fa-star" style="color: #415a5b; margin-right: 0.5rem;"></i> {{ trans('${translation}') }}`);
 
             if (v.rate !== undefined && v.rate.length > 0) { //  If v.rate is empty or undefined, the append operation will not be performed.
                 $('#bettingTypeContainer').append(bettingTypeContainerTemp);
