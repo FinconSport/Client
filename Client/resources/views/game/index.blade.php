d7fb784f549f5293947d7b0c3cd4bf5a24155ce3
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-16 14:58:33 +0800

    Update index.blade.php

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -693,7 +693,7 @@
     function renderViewV2() {
         if (matchListD.data.list.status === 1) {
             $('.marketName').css('background-color', '#c4d4d4');
-            $('#bettingTypeContainer').css('height', 'calc(100% - 15.5rem))');
+            $('#bettingTypeContainer').css('height', 'calc(100% - 15.5rem)');
         } else if (matchListD.data.list.status === 2) {
             $('.marketName').css('background-color', '#ffca9b');
             $('#bettingTypeContainer').css('height', 'calc(100% - 18.5rem)');
