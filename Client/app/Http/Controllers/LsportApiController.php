7730468257c6f60d07c5ec130386f43ecea87227
FinConSoft <fayi.pon@gmail.com>
2023-10-16 17:19:57 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -2076,9 +2076,12 @@ class LsportApiController extends Controller {
 
         ////////////////////////
         // gzip
-        $data = $this->gzip($data);
-
-        $this->ApiSuccess($data, "01", true);
+        if (!isset($input['is_gzip']) || ($input['is_gzip']==1)) {  // 方便測試觀察輸出可以開關gzip
+            $data = $this->gzip($data);
+            $this->ApiSuccess($data, "01", true);
+        } else {
+            $this->ApiSuccess($data, "01", false);
+        }
     }
 
     /**
