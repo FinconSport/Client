252b3dd6b13b1583c75e6314cb18a21cfd5fc80d
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-16 13:57:14 +0800

    Update index.blade.php

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -790,10 +790,19 @@
                     break;
             }
 
+            if (v2.status == 1) {
+                marketBetRateTemp.find('.fa-lock').hide();
+                marketBetRateTemp.attr('onclick', 'openCal($(this))');
+                marketBetRateTemp.find('.market_price').show();
+            } else {
+                marketBetRateTemp.find('.fa-lock').show();
+                marketBetRateTemp.removeAttr('onclick');
+                marketBetRateTemp.find('.market_price').hide();
+            }
+
             let fixture_id = matchListD.data.list.fixture_id;
             let currentPrice = parseFloat(marketBetRateTemp.attr('bet_rate'));
-            if (v2.status == 1) {
-                marketBetRateTemp.attr('onclick', 'openCal($(this))');
+            if (matchListD.data.list.status == 1) {
                 if (currentPrice > parseFloat(v2.price)) {
                     marketBetRateTemp.removeClass('lowerOdd');
                     marketBetRateTemp.find('.fa-caret-down').hide();
@@ -809,7 +818,6 @@
                     marketBetRateTemp.find('.fa-caret-down').hide();
                 }
             } else {
-                marketBetRateTemp.removeAttr('onclick');
                 marketBetRateTemp.removeClass('raiseOdd');
                 marketBetRateTemp.removeClass('lowerOdd');
                 marketBetRateTemp.find('.fa-caret-up').hide();
