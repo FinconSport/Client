b19c664c651e3ee9038b1e22b5145c57479ccf35
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-16 21:21:32 +0800

    Update index.blade.php

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -732,7 +732,7 @@
 
             if (!existingMarketIds.has(marketId)) {
                 // Remove the bet type
-                // $(this).remove();
+                $(this).remove();
                 console.log('Bet type with ID ' + this.id + ' is no longer present and has been removed.');
             }
         });
