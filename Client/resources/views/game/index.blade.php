9182509df35c4a3d72a6fa04cc6da6a1088a1d5a
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-17 16:35:05 +0800

    Update index.blade.php

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -487,16 +487,10 @@
         $('#bettingTypeContainer div[key="marketBetRateKey"]').each(function() {
             let priority = parseInt($(this).attr('priority'))
             let market_bet_id = parseInt($(this).attr('market_bet_id'))
-            let result = null;
+            let result = null
             let resultArr = matchListD.data?.list?.market?.find(item => item.priority === priority);
-            
-            if (resultArr && resultArr.market_bet) {
-                result = resultArr.market_bet[1].find(item => item.market_bet_id === market_bet_id);
-            }
-            
-            if (!result) {
-                $(this).remove();
-            }
+            if( resultArr.market_bet ) result = result = resultArr.market_bet[1]?.find(item => item.market_bet_id === market_bet_id);
+            if( !result ) $(this).remove()
         });
 
     }
