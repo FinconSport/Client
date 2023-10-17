83da4e72e7c318f92ce8a8b2078b913590f34d12
FinConSoft <fayi.pon@gmail.com>
2023-10-17 15:11:41 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -1984,7 +1984,7 @@ class LsportApiController extends Controller {
 
         //////////////////////////////////////////
 
-        if (isset($input['debug']) {
+        if (isset($input['debug'])) {
             $return = GameOrder::select('m_id')
             ->groupBy('m_id')
             ->orderBy('m_id', 'desc')
