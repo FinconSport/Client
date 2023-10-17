97a31596eab84f69d2cb8a243a6ca981f707ca8d
FinConSoft <fayi.pon@gmail.com>
2023-10-17 13:50:54 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -2039,7 +2039,8 @@ class LsportApiController extends Controller {
             "status"
         );
 
-        $round_columns = ['bet_amount','result_amount','active_bet'];
+        $round_columns = ['bet_amount','result_amount','active_bet','bet_rate','player_rate'];
+
 
         foreach ($order_data as $k => $v) {
             foreach ($columns as $kk => $vv) {
@@ -2055,7 +2056,7 @@ class LsportApiController extends Controller {
             
             // 關於小數點處理
             foreach ($round_columns as $kkkk => $vvvv) {
-                if (isset($tmp[$k][$vvvv]) && ($tmp[$k][$vvvv] != null)) {
+                if (isset($tmp[$k][$vvvv]) || ($tmp[$k][$vvvv] != null)) {
                     $tmp[$k][$vvvv] = round($tmp[$k][$vvvv],2);
                 }
             }
@@ -2081,7 +2082,7 @@ class LsportApiController extends Controller {
 
                     // 關於小數點處理
                     foreach ($round_columns as $kkkk => $vvvv) {
-                        if (isset($tmp_bet_data[$vvvv]) && ($tmp_bet_data[$vvvv] != null)) {
+                        if (isset($tmp_bet_data[$vvvv]) || ($tmp_bet_data[$vvvv] != null)) {
                             $tmp_bet_data[$vvvv] = round($tmp[$k][$vvvv],2);
                         }
                     }
@@ -2101,7 +2102,7 @@ class LsportApiController extends Controller {
 
                 // 關於小數點處理
                 foreach ($round_columns as $kkkk => $vvvv) {
-                    if (isset($tmp_bet_data[$vvvv]) && ($tmp_bet_data[$vvvv] != null)) {
+                    if (isset($tmp_bet_data[$vvvv]) || ($tmp_bet_data[$vvvv] != null)) {
                         $tmp_bet_data[$vvvv] = round($tmp[$k][$vvvv],2);
                     }
                 }
