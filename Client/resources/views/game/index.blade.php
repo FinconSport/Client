b0b3310b28bee645e778cfe8b561bfb1bd826f22
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-17 16:08:21 +0800

    Update index.blade.php

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -445,7 +445,7 @@
                             // new rate
                             bet_item.find('.odd').text(v3.price)
                             let pri = v.priority
-                            switch (pri) {
+                            switch (true) {
                                 case langTrans2.betTypePriority.hcapPriority.indexOf(pri) !== -1:
                                     marketBetRateTemp.find('.market_bet_name').text(v3.line);
                                     break;
@@ -456,11 +456,11 @@
                                     marketBetRateTemp.find('.market_bet_name').text(v3.market_bet_name);
                                     break;
                                 case langTrans2.betTypePriority.allWinPriority.indexOf(pri) !== -1:
-                                    if (v3.market_bet_name_en == 1) {
+                                    if (v3.market_bet_name_en === 1) {
                                         marketBetRateTemp.find('.market_bet_name').text(matchListD.data.list.home_team_name + ' ' + v3.line);
-                                    } else if (v3.market_bet_name_en == 2) {
+                                    } else if (v3.market_bet_name_en === 2) {
                                         marketBetRateTemp.find('.market_bet_name').text(matchListD.data.list.away_team_name + ' ' + v3.line);
-                                    } else if (v3.market_bet_name_en == 'X') {
+                                    } else if (v3.market_bet_name_en === 'X') {
                                         marketBetRateTemp.find('.market_bet_name').text("{{ trans('game.index.tie') }}");
                                     }
                                     break;
@@ -625,7 +625,7 @@
 
         marketBetRateTemp.find('.odd').text(v3.price)
         let pri = v.priority
-        switch (pri) {
+        switch (true) {
             case langTrans2.betTypePriority.hcapPriority.indexOf(pri) !== -1:
                 marketBetRateTemp.find('.market_bet_name').text(v3.line);
                 break;
@@ -636,11 +636,11 @@
                 marketBetRateTemp.find('.market_bet_name').text(v3.market_bet_name);
                 break;
             case langTrans2.betTypePriority.allWinPriority.indexOf(pri) !== -1:
-                if (v3.market_bet_name_en == 1) {
+                if (v3.market_bet_name_en === 1) {
                     marketBetRateTemp.find('.market_bet_name').text(matchListD.data.list.home_team_name + ' ' + v3.line);
-                } else if (v3.market_bet_name_en == 2) {
+                } else if (v3.market_bet_name_en === 2) {
                     marketBetRateTemp.find('.market_bet_name').text(matchListD.data.list.away_team_name + ' ' + v3.line);
-                } else if (v3.market_bet_name_en == 'X') {
+                } else if (v3.market_bet_name_en === 'X') {
                     marketBetRateTemp.find('.market_bet_name').text("{{ trans('game.index.tie') }}");
                 }
                 break;
