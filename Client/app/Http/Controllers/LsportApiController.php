b7b88eba6661e87451b7f76b20285e5e4326962d
FinConSoft <fayi.pon@gmail.com>
2023-10-21 07:22:36 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -1996,7 +1996,7 @@ class LsportApiController extends Controller {
                 $active_market_bet++;
             }
 
-            if (($active_market_bet > 0) && $check_market_bet_lines) {
+            if ($active_market_bet > 0) {
                 $data['list']['market'][] = $tmp_market_data;
             }
         }
