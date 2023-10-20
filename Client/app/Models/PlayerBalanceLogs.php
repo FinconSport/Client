a8dbbfb96c0b3f49d35fff1294d34bffbb84ced0
FinConSoft <fayi.pon@gmail.com>
2023-10-20 16:13:48 +0800

    Update PlayerBalanceLogs.php

--- Client/app/Models/PlayerBalanceLogs.php
+++ Client/app/Models/PlayerBalanceLogs.php
@@ -35,7 +35,7 @@ class PlayerBalanceLogs extends CacheModel
 				$return = PlayerBalanceLogs::where("player_id", $data['player'])
 				->where("create_time",">=",$data['start_time'])
 				->where("create_time","<",$data['start_time'])
-				->where("balance_type", $data['balance_type'])
+				->where("balance_type", "=", $data['balance_type'])
 				->skip($data['skip'])
 				->take($data['page_limit'])
 				->orderBy('id', 'DESC')
