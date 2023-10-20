4dd7900fc92f6016a0c5c5cce25e472a26caaf1e
FinConSoft <fayi.pon@gmail.com>
2023-10-20 20:16:56 +0800

    dd

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -678,7 +678,7 @@ class LsportApiController extends Controller {
 
         //取2天內賽事
         $today = time();
-        $after_tomorrow_es = $today + 1 * 24 * 60 * 60; 
+        $after_tomorrow_es = $today + 2 * 24 * 60 * 60; 
         $after_tomorrow_es = '"'.date('Y-m-d', $after_tomorrow_es).'T00:00:00"'; // 這個「"」不能拿掉, es會報錯
         $today_tomorrow_es = '"'.date('Y-m-d', $today).'T00:00:00"'; // 這個「"」不能拿掉, es會報錯
 
@@ -688,7 +688,7 @@ class LsportApiController extends Controller {
         $return = LsportFixture::query()
         ->from('es_lsport_fixture')
         ->where('start_time',">=", $today_tomorrow_es)
-        ->where('start_time',"<=", $after_tomorrow_es)
+        ->where('start_time',"<", $after_tomorrow_es)
         ->whereIn('status',[1,2,9])
         ->whereIn('sport_id', function($query) use ($sport_id) {
             $query->select('sport_id')
