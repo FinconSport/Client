b6fc7d895964ff62398a06e87dd4a4af55af22c1
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-23 11:52:11 +0800

    Update index.blade.php

--- Client/resources/views/match/index.blade.php
+++ Client/resources/views/match/index.blade.php
@@ -30,14 +30,14 @@
 						</div>
 					</div>
 				</div>
-				<div class="datebutton-cons">
-					<button class="dateCalendarBtn" data-range="lastMonth">{{ trans('common.search_area.last_month') }}</button>
-					<button class="dateCalendarBtn" data-range="lastWeek">{{ trans('common.search_area.last_week') }}</button>
-					<button class="dateCalendarBtn" data-range="yesterday">{{ trans('common.search_area.yesterday') }}</button>
-					<button class="dateCalendarBtn" data-range="today">{{ trans('common.search_area.today') }}</button>
-					<button class="dateCalendarBtn" data-range="thisWeek">{{ trans('common.search_area.this_week') }}</button>
-					<button class="dateCalendarBtn" data-range="thisMonth">{{ trans('common.search_area.this_month') }}</button>
-				</div>
+			</div>
+			<div class="datebutton-cons">
+				<button class="dateCalendarBtn" data-range="lastMonth">{{ trans('common.search_area.last_month') }}</button>
+				<button class="dateCalendarBtn" data-range="lastWeek">{{ trans('common.search_area.last_week') }}</button>
+				<button class="dateCalendarBtn" data-range="yesterday">{{ trans('common.search_area.yesterday') }}</button>
+				<button class="dateCalendarBtn" data-range="today">{{ trans('common.search_area.today') }}</button>
+				<button class="dateCalendarBtn" data-range="thisWeek">{{ trans('common.search_area.this_week') }}</button>
+				<button class="dateCalendarBtn" data-range="thisMonth">{{ trans('common.search_area.this_month') }}</button>
 			</div>
 		</div>
 	</div>
