c41b0bde2259e301e5b158fa8a1cc3a48935dee3
36peco <41123173+36peco@users.noreply.github.com>
2023-10-23 10:48:41 +0800

    Update common.js

--- Client/public/js/common.js
+++ Client/public/js/common.js
@@ -3,7 +3,7 @@ function navTo(page) {
     window.location.href = '/' + page;
 }
 
-const searchDate = (date) => {
+function searchDate(date) {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0'); // 月份从0开始，需要加1
     const day = String(date.getDate()).padStart(2, '0');
