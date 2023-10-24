37981039a96272e01f46dbc628e13a6f1376a344
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-10-24 10:20:49 +0800

    Update index.blade.php

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -583,9 +583,7 @@
 
             const stageText = commonLangTrans.stageArr[sport][data.list.periods.period];
             const TeamNameHead = $(`<th style="width: 25%; text-align: left;"><div class="setHeightDiv">${stageText}${stageStr}</div></th>`);
-            console.log("teamheadname" + stageText + stageStr);
-
-            scoreBoardHeadTemp.append(TeamNameHead);
+            scoreBoardHeadTemp.append(homeTeamName);
 
             let baseballShowStage = []
             for (let i = 0; i < gameTitle.length; i++) {
