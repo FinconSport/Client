76d49870ee3258b2c6e20b180fd5678d96f48b6d
36peco <41123173+36peco@users.noreply.github.com>
2023-10-24 14:27:34 +0800

    Update index.blade.php

--- Client/resources/views/index/index.blade.php
+++ Client/resources/views/index/index.blade.php
@@ -1303,11 +1303,11 @@
     // 最高可贏
     $('#moneyInput').on('keyup input change', function(event) {
         let inputMoney = parseInt($(this).val())
-        let min = parseInt($('#submitOrder').attr('min'))
-        let max = parseInt($('#submitOrder').attr('max'))
+        // let min = parseInt($('#submitOrder').attr('min'))
+        // let max = parseInt($('#submitOrder').attr('max'))
         if (isNaN(inputMoney)) inputMoney = ''
-        if (inputMoney < min) inputMoney = min
-        if (inputMoney > max) inputMoney = max
+        // if (inputMoney < min) inputMoney = min
+        // if (inputMoney > max) inputMoney = max
         let odd = parseFloat($('span[key="odd"]').html())
         let maxMoney = (inputMoney * odd).toFixed(2);
         $('#maxWinning').html(maxMoney)
