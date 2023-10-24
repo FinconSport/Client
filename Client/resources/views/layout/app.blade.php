f4e2d28af91b6cc7b3365d22ba4d74264f34af39
FinConSoft <fayi.pon@gmail.com>
2023-10-24 17:45:02 +0800

    Update app.blade.php

--- Client/resources/views/layout/app.blade.php
+++ Client/resources/views/layout/app.blade.php
@@ -164,7 +164,7 @@
 		// player and sport_id
 		const player = @json(session('player.id'));
 		const langText = @json(session('player.lang'));
-		const token = 12345
+		const token = @json(session('player.token'));
 		var sport = parseInt(searchData.sport)
 
 		// loading page control
