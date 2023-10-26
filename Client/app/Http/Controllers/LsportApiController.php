b072f0a7ea6a305bad6c272c1e0aacae2f2fb72f
FinConSoft <fayi.pon@gmail.com>
2023-10-26 11:08:49 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -748,6 +748,9 @@ class LsportApiController extends Controller {
             // fixture columns
             $columns = ["fixture_id","start_time","status","last_update"];
             foreach ($columns as $kk => $vv) {
+                if ($vv == "start_time") {
+                    $v[$vv] = date("Y-m-d H:i:s",$v[$vv]);
+                }
                 $data[$status_type_name][$sport_id]['list'][$league_id]['list'][$fixture_id][$vv] = $v[$vv];
             }
 
