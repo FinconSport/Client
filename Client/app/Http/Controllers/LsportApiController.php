f7693c33bfc88dc115400a0a7bdc87d7d343bc4e
FinConSoft <fayi.pon@gmail.com>
2023-10-26 10:50:39 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -2253,7 +2253,7 @@ class LsportApiController extends Controller {
 
                 // 滾球/早盤字樣判定
                 $market_type = 0;
-                dd($return['start_time'],$vvv['create_time']);
+                dd($return['start_time'],$v['create_time']);
                 if ($return['start_time'] < $v['create_time']) {
                     $market_type = 1;
                 }
