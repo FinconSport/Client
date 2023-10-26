25b5064e40365bca4f33a9c004bf524f2ae24cc8
FinConSoft <fayi.pon@gmail.com>
2023-10-26 10:17:08 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -2225,7 +2225,6 @@ class LsportApiController extends Controller {
 
                 $tmp_bet_data = $v;
                 $tmp_bet_data['start_time'] = date("Y-m-d H:i:s",strtotime($return['start_time']));
-                $tmp_bet_data['approval_time'] = date("Y-m-d H:i:s",strtotime($return['approval_time']));
                 $tmp_bet_data['result_time'] = date("Y-m-d H:i:s",strtotime($return['result_time']));
                 $tmp_bet_data['create_time'] = date("Y-m-d H:i:s",strtotime($return['create_time']));
 
