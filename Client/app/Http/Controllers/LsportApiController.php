944d4d14abe85a8faa2ef8e9bbd768a7b234d1ca
FinConSoft <fayi.pon@gmail.com>
2023-10-26 11:21:10 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -1994,7 +1994,7 @@ class LsportApiController extends Controller {
             // 取得market_bet
             $return = LsportMarketBet::where('fixture_id',$fixture_id)
             ->where("market_id",$market_id)
-            ->orderBy("name_en","ASC")
+            ->orderBy("name_en.keyword","ASC")
             ->list();
             if ($return === false) {
                 $this->ApiError('04');
