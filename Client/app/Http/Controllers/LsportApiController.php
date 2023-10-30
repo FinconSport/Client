35786aff44727406835736e850bba6a52b7978c9
FinConSoft <fayi.pon@gmail.com>
2023-10-31 06:27:15 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -537,7 +537,6 @@ class LsportApiController extends Controller {
                 $data['living']['total'] += $living_count;
             }
         }
-        dd($data);
 
         // gzip
         if (!isset($input['is_gzip']) || ($input['is_gzip']==1)) {  // 方便測試觀察輸出可以開關gzip
