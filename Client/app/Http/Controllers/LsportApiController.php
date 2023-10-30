567bf14217208af74663586d6d9adbfa0f729e86
FinConSoft <fayi.pon@gmail.com>
2023-10-31 06:32:23 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -443,9 +443,10 @@ class LsportApiController extends Controller {
             $return = Redis::hget('lsport_match_list', $key);
             $return = json_decode($return,true);
 
-
-            dd($return['early']);
-            $sport_name = LsportSport::getName(['sport_id'=>$sport_id, 'api_lang'=>$agent_lang]);
+            if ($sport_id  == 48242) {
+                dd($return['early']);
+            }
+            $sport_name = $return['sport_name'];
             $early_count = count($return['early'][$sport_id]['list']);
             $living_count = count($return['living'][$sport_id]['list']);
 
