1ea9bdb2f8a1d068172e2b63a3a564f017a0a903
FinConSoft <fayi.pon@gmail.com>
2023-10-31 09:22:51 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -1655,10 +1655,8 @@ class LsportApiController extends Controller {
             foreach ($redis_data as $k => $v) {
                 foreach ($v[$sport_id]['list'] as $kk => $vv) {
                     foreach ($vv['list'] as $kkk => $vvv) {
-
                         $fixture_id = $fixture_id+0;
-                        dd($kkk , $fixture_id);
-                        if ($kkk == intval($fixture_id)) {
+                        if ($kkk === $fixture_id) {
                             $pass = true;
                             $redis_fixture = $vvv;
 
