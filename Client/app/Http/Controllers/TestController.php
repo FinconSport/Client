07e227cc61e4bc2b0ac3b0de7e2cabdf40180027
Fayi <fayi.pon@gmail.com>
2023-11-10 09:27:23 +0800

    Update TestController.php

--- Client/app/Http/Controllers/TestController.php
+++ Client/app/Http/Controllers/TestController.php
@@ -14,35 +14,16 @@ class TestController extends PcController {
     
     // 首頁
     public function index(Request $request) {
-    
-      // list方法 , 同get方法
-      $return = Player::where("status",1)->where("currency_type",1)->list();
 
-      // fetch方法 , 同first 方法
-      $return = Player::where("status",1)->where("id",1)->where("currency_type",1)->fetch();
-
-      // cc方法 , 同count 方法
-      $return = Player::where("status",1)->where("currency_type",1)->cc();
-
-      // total方法, 專門用於取得統計
-      $return = Player::select('agent_id', DB::raw('SUM(balance) as total_balance'), DB::raw('COUNT(*) as player_count'))->groupBy('agent_id')->total();
-
-      // 多表關聯搜尋 , 需要用別名
-      $return = LsportFixture::select('f.*')
-      ->from('es_lsport_fixture as f')
-      ->join('es_lsport_sport as s', 'f.sport_id', '=', 's.sport_id')
-      ->where('s.status', '=', 1)
-      ->skip(100)->take(10)
-      ->orderBy("f.start_time","DESC")
+      $return = LsportMarketBet::where('fixture_id',$fixture_id)
+      ->where("market_id",$market_id)
+      ->orderBy("name_en.keyword","ASC")
       ->list();
 
       ////////////////////////
 
       dd($return);
   
-      if ($return === false) {
-
-      }
       
     }
 
