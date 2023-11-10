b48ce6eeecbdc26140143f525928576e392278ac
Fayi <fayi.pon@gmail.com>
2023-11-10 09:25:55 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -1736,7 +1736,6 @@ class LsportApiController extends Controller {
             // 取得market_bet
             $return = LsportMarketBet::where('fixture_id',$fixture_id)
             ->where("market_id",$market_id)
-            ->where('provder_bet_id', 8)
             ->orderBy("name_en.keyword","ASC")
             ->list();
             if ($return === false) {
