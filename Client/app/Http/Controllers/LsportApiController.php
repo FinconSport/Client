d130edbc4f272eee5abcea7ba174c8fb05985406
Fayi <fayi.pon@gmail.com>
2023-11-20 14:13:48 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -324,84 +324,10 @@ class LsportApiController extends Controller {
             ];
         }
 
-        //---------------------------------
-        // 自DB取出LsportNotice
-        $week_before =  date('Y-m-d 00:00:00', strtotime('-1 week'));
-        $return = LsportNotice::getList(["create_time" => $week_before]);
-        if ($return === false) {
-            $this->ApiError("02");
-        }
-
-        $notice_data = $return;
-
-        foreach ($notice_data as $k => $v) {
-            $sport_id = $v['sport_id'];
-            $league_id = $v['league_id'];
-            $fixture_id = $v['fixture_id'];
-            $notice_type = $v['type'];
-
-            // sport -----
-            $sport_name = LsportSport::getName(['sport_id'=>$sport_id, 'api_lang'=>$agent_lang]);
-
-            // league -----
-            $league_name = LsportLeague::getName(['league_id'=>$league_id, 'api_lang'=>$agent_lang]);
-
-            // fixture -----
-            $return = LsportFixture::where('fixture_id', $fixture_id)->fetch();
-            if ($return === false) {
-                $this->ApiError("02");
-            }
-
-            $fixture = $return;
-            $fixture_start_time = $fixture['start_time'];
-            $home_team_id = $fixture['home_id'];
-            $away_team_id = $fixture['away_id'];
-
-            // team: home team -----
-            $home_team_name = LsportTeam::getName(['team_id'=>$home_team_id, 'api_lang'=>$agent_lang]);
-
-            // team: away team -----
-            $away_team_name = LsportTeam::getName(['team_id'=>$away_team_id, 'api_lang'=>$agent_lang]);
-
-            // 處理 Duplication of <FIXTURE_ID> 的翻譯問題
-            if (strpos($notice_type, 'Duplication of') !== false) {
-                $arr_notice_type = explode(' ', $notice_type);
-                $notice_type = "{$arr_notice_type[0]} {$arr_notice_type[1]}";
-                $fixture_id = $arr_notice_type[2];
-            }
-    
-            $title = trans('notice.fixture_cancellation_reasons.'.'title:'.$notice_type, [
-                'sport_name' => $sport_name,
-                'league_name' => $league_name,
-            ]);
-            
-            $fixture_start_time2 = date(
-                trans('notice.fixture_cancellation_reasons.date_time_to_hour'),
-                strtotime($fixture_start_time)
-            );
-
-            $context = trans('notice.fixture_cancellation_reasons.'.$notice_type, [
-                'sport_name' => $sport_name,
-                'league_name' => $league_name,
-                'fixture_start_time' => $fixture_start_time2,
-                'home_team_name' => $home_team_name,
-                'away_team_name' => $away_team_name,
-                'fixture_id' => $fixture_id,
-            ]);
-
-            $create_time = $v['create_time'];
-    
-            $notice_list[$sport_id][] = [
-                "sport_id" => $sport_id,
-                "title" => $title,
-                "context" => $context,
-                "create_time" => $create_time,
-            ];
-        }
-
         ///////////////////////////////////
 
         $data = $notice_list;
+        
         // gzip
         if (!isset($input['is_gzip']) || ($input['is_gzip']==1)) {  // 方便測試觀察輸出可以開關gzip
             $data = $this->gzip($data);
