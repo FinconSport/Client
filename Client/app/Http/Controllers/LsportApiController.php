896c764cfef04dd99ce4e8944d172a1c0d53c789
Fayi <fayi.pon@gmail.com>
2023-11-23 09:53:14 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -15,6 +15,7 @@ use App\Models\LsportTeam;
 use App\Models\LsportMarket;
 use App\Models\LsportMarketBet;
 use App\Models\LsportNotice;
+use App\Models\LsportRisk;
 
 use App\Models\PlayerOnline;
 use App\Models\Player;
