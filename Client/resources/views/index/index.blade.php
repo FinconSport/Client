36d9daaaf5cf7bae602816a56c54c3d26ae7a87d
36peco <41123173+36peco@users.noreply.github.com>
2023-11-24 09:13:10 +0800

    test

--- Client/resources/views/index/index.blade.php
+++ Client/resources/views/index/index.blade.php
@@ -911,7 +911,7 @@
                                         item.find('.odd').html(v4.price)
 
                                         // 賦值
-                                        if( hcapArr.indexOf(i) !== -1 ) item.find('.bet_name').html( v4.line )
+                                        if( hcapArr.indexOf(i) !== -1 ) item.find('.bet_name').html( '-0/0.5(2-0)' )
                                         if( sizeArr.indexOf(i) !== -1 ) item.find('.bet_name').html('<span class="bet_name_item">'+v4.market_bet_name+'</span>' + v4.line)
                                         if( oddEvenArr.indexOf(i) !== -1 ) item.find('.bet_name').html( '<span class="bet_name_item">'+v4.market_bet_name+'</span>' )
                                         if( allWinArr.indexOf(i) !== -1 && sport === 6046 ) item.find('.bet_name').html( '<span class="bet_name_item">'+v4.market_bet_name+'</span>' )
