71fcdaccd066b10f625bfb3be5b8ecc1f9d37cfe
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-11-27 11:57:12 +0800

    Update index.blade.php

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -692,15 +692,14 @@
     // ------- game page scoreboard function-----------
     function createScoreBoard(data) {
         const earlyContainerTemp = $('div[template="earlyContainerTemplate"]').clone();
-        
+        const scoreBoardHeadTemp = $('tr[template="scoreBoardHeadTemplate"]').clone();
+        const scoreBoardBodyTemp_home = $('tr[template="scoreBoardBodyTemplate_home"]').clone();
+        const scoreBoardBodyTemp_away = $('tr[template="scoreBoardBodyTemplate_away"]').clone();
 
         if ((data.list.status == 2 || data.list.status == 9) && data.list.scoreboard) {
             if (sport === 154914) {
                 for (let i = 0; i < 3; i++) {
                     const livingContainerTemp = $('div[template="livingContainerTemplate"]').clone();
-                    const scoreBoardHeadTemp = $('tr[template="scoreBoardHeadTemplate"]').clone();
-                    const scoreBoardBodyTemp_home = $('tr[template="scoreBoardBodyTemplate_home"]').clone();
-                    const scoreBoardBodyTemp_away = $('tr[template="scoreBoardBodyTemplate_away"]').clone();
                     
                     livingContainerTemp.removeAttr('hidden').removeAttr('template');
                     $('div[key="livingContainerTemplate"]').removeAttr('hidden');
@@ -800,9 +799,6 @@
             } else {
                 // The rest of the code for the case when sport is not 154914
                 const livingContainerTemp = $('div[template="livingContainerTemplate"]').clone();
-                const scoreBoardHeadTemp = $('tr[template="scoreBoardHeadTemplate"]').clone();
-                const scoreBoardBodyTemp_home = $('tr[template="scoreBoardBodyTemplate_home"]').clone();
-                const scoreBoardBodyTemp_away = $('tr[template="scoreBoardBodyTemplate_away"]').clone();
 
                 livingContainerTemp.removeAttr('hidden').removeAttr('template');
                 $('div[key="livingContainerTemplate"]').removeAttr('hidden');
