e4625d45480e69d608d1d3901e5a078c4c303cd8
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-11-28 00:05:01 +0800

    Update index.blade.php

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -64,6 +64,7 @@
 <!-- early & living scoreboard-->
     <!-- template clone -->
         <!-- early fixture -->
+    <div class="template-con">
         <div class="earlyFixture-container row" template="earlyContainerTemplate" hidden>
             <p class="home_team_name col-3"></p>
             <div class="col-4">
@@ -108,6 +109,7 @@
                 </tbody>
             </table>
         </div>
+    </div>
     <!-- template clone -->
 
 <div id="scoreboardContainer">
@@ -727,18 +729,20 @@
             if (sport === 154914) {
                 const scbLen = data.list?.scoreboard[1].length - 1;
 
-                document.querySelectorAll(".early-fixture").forEach(el => {
-                    el.style.display = "none";
-                });
+                $('.template-con').append('.early-fixture-con');
 
                 createScoreBoardTemplate(sport, data, [0, 1, 2, 3, 4, 5, 6]);
 
                 if (scbLen >= 6) {
                     isBaseball_createScoreBoardTemplate_moreThanSix(sport, data, [0, 4, 5, 6, 7, 8, 9]);
+                } else {
+                    $('.template-con').append('.living-fixture-isBaseball-mts');
                 }
 
                 if (scbLen > 9) {
                     isBaseball_createScoreBoardTemplate_moreThanNine(sport, data, [0, 7, 8, 9, 10, 11, 12]);
+                } else {
+                    $('.template-con').append('.living-fixture-isBaseball-mtn');
                 }
 
             } else {
