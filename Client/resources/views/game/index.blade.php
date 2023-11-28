b4ad9521a2d5e0fcc4871e34cd300b532e96d225
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-11-28 14:19:40 +0800

    Update index.blade.php

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -779,7 +779,7 @@
             if (sport === 154914) {
                 const scbLen = data.list?.scoreboard[1].length - 1;
                 // remove early slide
-                removeAndAppend('.early-fixture-con', 'none');
+                removeAndAppend('.early-fixture-con, .living-fixture-mtn, .living-fixture-mts', 'none');
                 
                 createScoreBoardTemplate(sport, data, [0, 1, 2, 3, 4, 5, 6, 4, 5, 6, 7, 8, 9, 10, 11, 12], "lts");
                 // if (scbLen >= 6) {
