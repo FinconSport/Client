a50dd0f176e3a7a09f51f7c57d3d5b250523d8f0
Fayi <fayi.pon@gmail.com>
2023-12-06 11:04:30 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -640,8 +640,7 @@ class LsportApiController extends Controller {
                             }
 
                             if ($market_id == 226) {
-                                $market_data = $data[$k][$sport_id]['list'][$league_id]['list'][$fixture_id]['list'][$market_id];
-                                dd( $market_data , $risk_data[$market_id]);
+                                dd($data[$k][$sport_id]['list'][$league_id]['list'][$fixture_id]['list'][$market_id] , $risk_data[$market_id]);
                             }
                         }
                     }
