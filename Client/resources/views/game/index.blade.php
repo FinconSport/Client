bee1090370e728e6732ec1341b7acac059e2ea61
36peco <41123173+36peco@users.noreply.github.com>
2023-12-08 11:25:34 +0800

    Update index.blade.php

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -345,14 +345,15 @@
                             let oldRate = parseFloat(bet_item.attr('bet_rate'))
                             let newRate = parseFloat(v3.price)
 
+                            console.log(oldRate, newRate, v3.market_bet_name, v3.line)
                             // status
                             if( v3.status === 1 ) {
                                 bet_item.find('.fa-lock').hide()
                                 bet_item.attr('onclick', 'openCal($(this))')
                                 
                                 // rate compare
-                                if( oldRate > newRate ) lowerOdd(v.priority, v3.market_bet_id), console.log(oldRate, newRate, v3.market_bet_name, v3.line)
-                                if( oldRate < newRate ) raiseOdd(v.priority, v3.market_bet_id), console.log(oldRate, newRate, v3.market_bet_name, v3.line)
+                                if( oldRate > newRate ) lowerOdd(v.priority, v3.market_bet_id)
+                                if( oldRate < newRate ) raiseOdd(v.priority, v3.market_bet_id)
                             } else {
                                 bet_item.find('.fa-lock').show()
                                 bet_item.removeAttr('onclick')
