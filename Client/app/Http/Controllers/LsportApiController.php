31eb012426ab4143b921bcb45ef1edbb9945d480
Fayi <fayi.pon@gmail.com>
2023-12-09 15:08:52 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -318,7 +318,7 @@ class LsportApiController extends Controller {
             $context = $v['marquee'];
             $create_time = $v['create_time'];
 
-            $notice_list[] = [
+            $notice_list[0][] = [
                 "sport_id" => $sport_id,
                 "title" => $title,
                 "context" => $context,
