86da692442fdc3cb8fefb640458c9db77d17b1d7
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-12-09 16:33:35 +0800

    change current time into $system_config['version']

--- Client/resources/views/order/index.blade.php
+++ Client/resources/views/order/index.blade.php
@@ -127,8 +127,7 @@
 @endsection
 
 @section('styles')
-<!-- <link href="{{ asset('css/order.css?v=' . $system_config['version']) }}" rel="stylesheet"> -->
-<link href="{{ asset('css/order.css?v=' . $current_time) }}" rel="stylesheet">
+<link href="{{ asset('css/order.css?v=' . $system_config['version']) }}" rel="stylesheet">
 <style>	
 /* 寫入頁面限定CSS */
 </style>
