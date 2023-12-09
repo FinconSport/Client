505fbda2df2e3571670348b087cc1b6f822e409d
Fayi <fayi.pon@gmail.com>
2023-12-09 15:10:24 +0800

    Update LsportApiController.php

--- Client/app/Http/Controllers/LsportApiController.php
+++ Client/app/Http/Controllers/LsportApiController.php
@@ -324,6 +324,17 @@ class LsportApiController extends Controller {
                 "context" => $context,
                 "create_time" => $create_time,
             ];
+
+            if ($sport_id != 0) {
+                $notice_list[0][] = [
+                    "sport_id" => $sport_id,
+                    "title" => $title,
+                    "context" => $context,
+                    "create_time" => $create_time,
+                ];
+    
+
+            }
         }
 
         ///////////////////////////////////
