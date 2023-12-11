9917b553021c9bee3f35ca2a97afb12bda9b3f0c
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-12-11 10:47:01 +0800

    Update index.blade.php

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -785,7 +785,7 @@
                 $(".early-fixture-con").addClass("d-none");
                 $(".navigation-controls").addClass("d-none");
                 createScoreBoardTemplate(sport, data, [0, 1, 2, 3, 4, 5, 6]);
-                const scoreboardLength = = fixtureData.scoreboard["1"].length;
+                const scoreboardLength = fixtureData.scoreboard["1"].length;
                 console.log('scoreboard legnth: ' + scoreboardLength);
             }
         } else {
