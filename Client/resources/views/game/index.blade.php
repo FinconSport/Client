993863e9359e6dc0db056af388156d823430d8c4
krstndncbstlls <krstndncbstlls0614@gmail.com>
2023-12-11 11:32:45 +0800

    reset code(test data)

--- Client/resources/views/game/index.blade.php
+++ Client/resources/views/game/index.blade.php
@@ -751,34 +751,21 @@
     function createScoreBoard(data) {
         const earlyContainerTemp = $('div[template="earlyContainerTemplate"]').clone();
         if ((data.status == 2 || data.status == 9) && data.scoreboard) {
-            if (sport === 131506) {
+            if (sport === 154914) {
                 const scbLen = data.scoreboard[1].length - 1;
                 $(".early-fixture-con").addClass("d-none");
 
                 createScoreBoardTemplate(sport, data, [0, 1, 2, 3, 4, 5, 6, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
                 $('.isBsbll:not(.isBsbll_0)').addClass("slider-bsbll");
 
-                // $(".isBsbll_1, .isBsbll_2, .isBsbll_3, .isBsbll_4, .isBsbll_5, .isBsbll_6").addClass("slide-1");
-                // if (scbLen >= 6) {
-                //     $(".isBsbll_4, .isBsbll_5, .isBsbll_6, .isBsbll_7, .isBsbll_8, .isBsbll_9").addClass("slide-2");
-                // } else {
-                //     $(".pgntn-bullet-2").addClass("d-none");
-                // }
-                // if (scbLen > 9) {
-                //     $(".isBsbll_7, .isBsbll_8, .isBsbll_9").addClass("slide-3");
-                // } else {
-                //     $(".pgntn-bullet-3").addClass("d-none");
-                // }
-                
-                //for debug only
-                $(".isBsbll_1, .isBsbll_2").addClass("slide-1");
-                if (scbLen >= 2) {
-                    $(".isBsbll_2, .isBsbll_3").addClass("slide-2");
+                $(".isBsbll_1, .isBsbll_2, .isBsbll_3, .isBsbll_4, .isBsbll_5, .isBsbll_6").addClass("slide-1");
+                if (scbLen >= 6) {
+                    $(".isBsbll_4, .isBsbll_5, .isBsbll_6, .isBsbll_7, .isBsbll_8, .isBsbll_9").addClass("slide-2");
                 } else {
                     $(".pgntn-bullet-2").addClass("d-none");
                 }
-                if (scbLen > 4) {
-                    $(".isBsbll_3, .isBsbll_4").addClass("slide-3");
+                if (scbLen > 9) {
+                    $(".isBsbll_7, .isBsbll_8, .isBsbll_9").addClass("slide-3");
                 } else {
                     $(".pgntn-bullet-3").addClass("d-none");
                 }
@@ -788,22 +775,10 @@
                 if (storedSlide) {
                     currentSlide = parseInt(storedSlide);
                 } else {
-                    // if (scbLen < 6) {
-                    //     lastslide = 1;
-                    //     currentSlide = 1;
-                    // } else if (scbLen >= 6 && scbLen <= 9) {
-                    //     lastslide = 2;
-                    //     currentSlide = 2;
-                    // } else {
-                    //     lastslide = 3;
-                    //     currentSlide = 3;
-                    // }
-
-                    //for debug only
-                    if (scbLen < 2) {
+                    if (scbLen < 6) {
                         lastslide = 1;
                         currentSlide = 1;
-                    } else if (scbLen >= 2 && scbLen <= 4) {
+                    } else if (scbLen >= 6 && scbLen <= 9) {
                         lastslide = 2;
                         currentSlide = 2;
                     } else {
@@ -875,7 +850,7 @@
         const gameTitle = gameLangTrans.scoreBoard.gameTitle[sport];
         // Thead data game title
         let stageStr = '';
-        if (sport === 131506 && data?.periods?.period < 10) {
+        if (sport === 154914 && data?.periods?.period < 10) {
             data.periods.Turn === '1' ? (stageStr = gameLangTrans.scoreBoard.lowerStage) : (stageStr = gameLangTrans.scoreBoard.upperStage);
         }
 
@@ -892,7 +867,7 @@
         let baseballShowStageTemp = baseballShowStage;
 
         for (let i = 0; i < gameTitle.length; i++) {
-            if (sport === 131506) {
+            if (sport === 154914) {
                 const scbLen = data.scoreboard[1].length - 1;
                 baseballShowStageTemp = baseballShowStage;
                 if (baseballShowStageTemp.indexOf(i) !== -1) {
@@ -910,8 +885,8 @@
         scoreBoardBodyTemp_home.append(homeTeamName);
         for (let i = 0; i < gameTitle.length; i++) {
             const scoreValue = Array.from(Object.values(scorehome))[i];
-            const thHome = $(`<td class="${sport === 131506 ? 'isBsbll isBsbll_' + i : ''}" style="width:10%;text-align:center;">`).text(scoreValue !== undefined ? scoreValue : '-');
-            if (!(sport === 131506 && baseballShowStage.indexOf(i) === -1)) {
+            const thHome = $(`<td class="${sport === 154914 ? 'isBsbll isBsbll_' + i : ''}" style="width:10%;text-align:center;">`).text(scoreValue !== undefined ? scoreValue : '-');
+            if (!(sport === 154914 && baseballShowStage.indexOf(i) === -1)) {
                 scoreBoardBodyTemp_home.append(thHome);
             }
         }
@@ -923,8 +898,8 @@
         scoreBoardBodyTemp_away.append(awayTeamName);
         for (let i = 0; i < gameTitle.length; i++) {
             const scoreValue = Array.from(Object.values(scoreaway))[i];
-            const thAway = $(`<td class="${sport === 131506 ? 'isBsbll isBsbll_' + i : ''}" style="width:10%;text-align:center;">`).text(scoreValue !== undefined ? scoreValue : '-');
-            if (!(sport === 131506 && baseballShowStage.indexOf(i) === -1)) {
+            const thAway = $(`<td class="${sport === 154914 ? 'isBsbll isBsbll_' + i : ''}" style="width:10%;text-align:center;">`).text(scoreValue !== undefined ? scoreValue : '-');
+            if (!(sport === 154914 && baseballShowStage.indexOf(i) === -1)) {
                 scoreBoardBodyTemp_away.append(thAway);
             }
         }
